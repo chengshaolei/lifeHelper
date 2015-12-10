9f30ecc735b59cba18edc0666293f4c346a5a03d
chengshaolei <landmark_csl@126.com>
2015-12-10 10:51:27 +0800

    纠正多次扫描问题

--- LiveAssistant/LiveAssistant/Controller/commontool/CSLScanViewController.m
+++ LiveAssistant/LiveAssistant/Controller/commontool/CSLScanViewController.m
@@ -9,13 +9,19 @@
 #import "QRCodeReaderViewController.h"
 #import "QRCodeReader.h"
 
+//静态指针
 static QRCodeReaderViewController *vc = nil;
+
 @interface CSLScanViewController ()
+
 -(void) createScanViewController;//创建扫描控制器
 - (void)scanAction;//扫描
 @end
 
 @implementation CSLScanViewController
+{
+    BOOL _isScanFinished;//判断扫描是否结束(YES)，默认没有
+}
 
 -(void) viewDidLoad{
     [super viewDidLoad];
@@ -24,50 +30,48 @@ static QRCodeReaderViewController *vc = nil;
 }
 -(void) viewDidAppear:(BOOL)animated{
     [super viewDidAppear:animated];
+    if (!_isScanFinished) {//如果扫描没结束
+        [self scanAction];
+    }
     
-    [self scanAction];
 }
 -(void) createScanViewController{
+    //判断设备是否支持二维码扫描
     if ([QRCodeReader supportsMetadataObjectTypes:@[AVMetadataObjectTypeQRCode]]) {
-        
+        //使用线程安全的方式创建一个单例
         static dispatch_once_t onceToken;
-        
-        dispatch_once(&onceToken, ^{
+        dispatch_once(&onceToken, ^{//只执行一次
+            //设置扫描类型为二维码
             QRCodeReader *reader = [QRCodeReader readerWithMetadataObjectTypes:@[AVMetadataObjectTypeQRCode]];
+            
+            //创建扫描控制器
             vc  = [QRCodeReaderViewController readerWithCancelButtonTitle:@"Cancel" codeReader:reader startScanningAtLoad:YES showSwitchCameraButton:YES showTorchButton:YES];
-            vc.modalPresentationStyle = UIModalPresentationFormSheet;
-        });
-//        vc.delegate = self;
+         });
     }
-    else {
+    else {//不支持
+        //弹框通知
         [Auxiliary alertWithTitle:NSLocalizedString(@"alertTitle", nil) message:NSLocalizedString(@"scanReaderNotSupport", nil) button:1 done:nil];
     }
 }
 - (void)scanAction
 {
-    //    [self presentViewController:vc animated:YES completion:NULL];
-    [self.view addSubview:vc.view];
+    //把扫描控制器的view加到当前的view上，不采用模态视图控制器方式操作
+     [self.view addSubview:vc.view];
+    
+    //避免循环引用
     __weak CSLScanViewController*weakSelf = self;
+    
+    //扫描完成后回自动回调block
+    //因为扫描过快，会发生多次扫描，会多次调用block
     [vc setCompletionWithBlock:^(NSString * _Nullable resultAsString) {
-        [Auxiliary alertWithTitle:NSLocalizedString(@"alertTitle", nil) message:resultAsString button:1 done:^{
-            [weakSelf.navigationController popViewControllerAnimated:YES];
-        }];
+        [vc stopScanning];//停止扫描
+        if (!_isScanFinished) {//使用_isScanFinished避免多次调用
+            _isScanFinished = YES;//置标志为真
+            [Auxiliary alertWithTitle:NSLocalizedString(@"alertTitle", nil) message:resultAsString button:1 done:^{
+                    [weakSelf.navigationController popViewControllerAnimated:YES];
+            }];
+        }
     }];
 }
-
-#pragma mark - QRCodeReader Delegate Methods
-//取得扫描结果
-//- (void)reader:(QRCodeReaderViewController *)reader didScanResult:(NSString *)result
-//{
-//    NSLog(@"%@",result);
-//    [reader stopScanning];
-//    [self.navigationController popViewControllerAnimated:YES];
-//}
-//
-////按下cancel按钮
-//- (void)readerDidCancel:(QRCodeReaderViewController *)reader
-//{
-//    [self.navigationController popViewControllerAnimated:YES];
-//}
 
 @end
